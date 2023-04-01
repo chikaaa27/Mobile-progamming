b797f17e2fadfce821bcf2526d1a83b06a7c6670
chikaaa27 <fadilachikkusumarini@gmail.com>
2023-04-01 11:28:52 +0700

    pegawai dan pasien

---
+++ lib/model/database.dart
@@ -0,0 +1,64 @@
+List<Map> pegawai = [
+  {
+    'id': 1,
+    'nip': '00124',
+    'nama': 'Farahdina',
+    'tanggalLahir': '13 November 1998',
+    'nomorTelepon': '081789725264',
+    'email': 'farah@gmail.com',
+    'password': '111',
+  },
+  {
+    'id': 2,
+    'nip': '00125',
+    'nama': 'Galang Triano',
+    'tanggalLahir': '8 Maret 1998',
+    'nomorTelepon': '081253647282',
+    'email': 'galang@gmail.com',
+    'password': '122',
+  },
+  {
+    'id': 3,
+    'nip': '00126',
+    'nama': 'Yuli Susana',
+    'tanggalLahir': '3 Agustus 1979',
+    'nomorTelepon': '085698163748',
+    'email': 'yuli@gmail.com',
+    'password': '123',
+  },
+];
+
+List<Map> pasien = [
+  {
+    'id': 1,
+    'noRm': '12210974',
+    'nama': 'Fadhillah Chika',
+    'tanggalLahir': '27 Januari 2002',
+    'nomorTelepon': '081229161415',
+    'alamat': 'Klaten',
+  },
+  {
+    'id': 2,
+    'noRm': '12210054',
+    'nama': 'Brigita Alasavista',
+    'tanggalLahir': '28 September 2002',
+    'nomorTelepon': '08771273617',
+    'alamat': 'Wonogiri',
+  },
+  {
+    'id': 3,
+    'noRm': '12210078',
+    'nama': 'Cahyo Yulianto',
+    'tanggalLahir': '12 November 2000',
+    'nomorTelepon': '08987273682',
+    'alamat': 'Tangerang',
+  },
+  {
+    'id': 4,
+    'noRm': '12210987',
+    'nama': 'Bayu Septiano',
+    'tanggalLahir': '7 Desember 20000',
+    'nomorTelepon': '0898238942',
+    'alamat': 'Yogyakarta',
+  },
+];
